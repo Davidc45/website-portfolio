055f0267274306159adb88e3fcf95b4c9cc22b8d
David Calderon <dabidcalderon45@gmail.com>
2023-06-29 02:35:10 -0700

    fixed navbar and added main content text

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#596464',
+          200: '#546496',
+          300: '#964d54',
+          400: '#2E3643',
+          500: '#1F2634'
+        }
+      },
+      fontFamily:
+      {
+        body: ['Lexend Peta']
+      }
+    },
   },
   plugins: [],
 }
